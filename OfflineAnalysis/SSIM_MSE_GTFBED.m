71a7f8b02814411c010392c32be895cf19d13407
GiuliaDAngelo <giulia.dangelo.1990@gmail.com>
2023-09-13 11:34:08 +0200

    refactoring and optimisation of the code + figure ground maps corrected for the comparison

--- OfflineAnalysis/SSIM_MSE_GTFBED.m
+++ OfflineAnalysis/SSIM_MSE_GTFBED.m
@@ -7,11 +7,105 @@ clc,clear all,close all;
 % imgs_generation();
 
 %% analysis on the FB-GT-ED computing SSIM & MSE
-ssim_mse();
+% ssim_mse();
 
 %% PLOT analysis on the FB-GT-ED computing SSIM & MSE
 
 
+%load name figures
+namefigures=load("namefigures.mat");
+namefigures=namefigures.namefigures;
+labelsfigures=load('labelsfigures.mat');
+labelsfigures=labelsfigures.labelsfigures;
+
+%load FB-ED data
+FBEDFGssimvals= load('EDFBssimvals.mat');
+FBEDFGssimvals=FBEDFGssimvals.EDFBssimvals;
+FBEDFGssimvals_mean=mean(FBEDFGssimvals);
+FBEDFGssimvals_var=var(FBEDFGssimvals);
+FBEDFGssimvals_mean= ones(1,length(FBEDFGssimvals))*FBEDFGssimvals_mean;
+[EDFBvalsort,FBEDindx] = sort(FBEDFGssimvals,'descend');
+EDFBlabelnames=namefigures(FBEDindx);
+
+EDFBmserrors=load('EDFBmserrors.mat');
+EDFBmserrors=EDFBmserrors.EDFBmserrors;
+EDFBmserrors=EDFBmserrors(FBEDindx);
+
+figure;
+subplot(3,1,1);
+plot_comp(EDFBlabelnames,EDFBvalsort, FBEDFGssimvals_mean,FBEDFGssimvals_var,'EDvsFB');
+
+%load FB-GT data
+FBGTssimvals= load('FBGTssimvals.mat');
+FBGTssimvals=FBGTssimvals.FBGTssimvals;
+FBGTssimvals_mean=mean(FBGTssimvals);
+FBGTssimvals_var=var(FBGTssimvals);
+FBGTssimvals_mean= ones(1,length(FBGTssimvals))*FBGTssimvals_mean;
+[FBGTvalsort,FBGTindx] = sort(FBGTssimvals,'descend');
+FBGTlabelnames=namefigures(FBGTindx);
+
+FBGTmserrors=load('FBGTmserrors.mat');
+FBGTmserrors=FBGTmserrors.FBGTmserrors;
+FBGTmserrors=FBGTmserrors(FBGTindx);
+    
+subplot(3,1,2);
+plot_comp(FBGTlabelnames,FBGTvalsort, FBGTssimvals_mean,FBGTssimvals_var,'FBvsGT');
+
+%load ED-GT data
+EDGTssimvals= load('EDGTssimvals.mat');
+EDGTssimvals=EDGTssimvals.EDGTssimvals;
+EDGTssimvals_mean=mean(EDGTssimvals);
+EDGTssimvals_var=var(EDGTssimvals);
+EDGTssimvals_mean= ones(1,length(EDGTssimvals))*EDGTssimvals_mean;
+[EDGTvalsort,EDGTindx] = sort(EDGTssimvals,'descend');
+EDGTlabelnames=namefigures(EDGTindx);
+
+EDGTmserrors=load('EDGTmserrors.mat');
+EDGTmserrors=EDGTmserrors.EDGTmserrors;
+EDGTmserrors=EDGTmserrors(EDGTindx);
+
+
+subplot(3,1,3);
+plot_comp(EDGTlabelnames,EDGTvalsort, EDGTssimvals_mean,EDGTssimvals_var,'EDvsGT');
+
+disp('end');
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%    FUNCTION   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function plot_comp(labelnames,valsort,ssimvals_mean,ssimvals_var, comp)
+    %%bar
+    x=categorical(labelnames);
+    x = reordercats(x,string(x));
+    y=valsort(1,:);
+    ax=gca;
+    bar(x,y,0.8,'FaceColor',[0.9290 0.6940 0.1250],'EdgeColor',[0 0 0],'LineWidth',1.5);
+    ylim([0 1])
+    hold on;
+    % curve1 = ssimvals_mean + ssimvals_var;
+    % curve2 = ssimvals_mean - ssimvals_var;
+    % x2 = [x, fliplr(x)];
+    % inBetween = [curve1, fliplr(curve2)];
+    % f=fill(x2, inBetween, 'yellow');
+    % alpha(f,.5)
+    % hold on;
+    
+    %%mean
+    plot(x,ssimvals_mean,'LineWidth',3);
+    
+    legend(comp,'mean',FontSize=20);
+    xtickangle(45)
+    ax.XAxis.FontSize = 15;
+    ax.YAxis.FontSize = 26;
+    x0=10;
+    y0=10;
+    width=10000;
+    height=1000;
+    set(gcf,'position',[x0,y0,width,height]);
+    set(gcf,'units','points','position',[x0,y0,width,height]);
+    hold off;
+end
 
 
 function ssim_mse()
@@ -106,12 +200,11 @@ function imgs_generation()
     disp(len_names);
     namefigures=string((len_names));
     labelsfigures=string((len_names));
-    cnt = 1;
     for idx=1:len_names
         disp(idx)
         name = extractBetween(names_str(idx).name , '-' , '-' );
-        namefigures(cnt)=name;
-        labelsfigures(cnt)=strcat('# ',int2str(idx));
+        namefigures(idx)=name;
+        labelsfigures(idx)=strcat('# ',int2str(idx));
         img_gt=rgb2gray(imread(strcat(path_gt,names_str(idx).name)));
         img_fbfg=rgb2gray(imread(strcat(path_FBFG,name{1},'.jpg')));
         img_edfg=rgb2gray(imread(strcat(path_EDFG,'FG_',name{1},'.png')));
@@ -122,7 +215,6 @@ function imgs_generation()
         img_edfg = imresize(img_edfg,[rows cols]);
         % generate images with legend or not, be careful to change it inside
         generate_imgsGT(img_gt,img_fbfg,img_edfg, name{1});
-        cnt=cnt+1;
     end
     save('namefigures.mat','namefigures');
     save('labelsfigures.mat','labelsfigures');
